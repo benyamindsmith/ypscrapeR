e156d28cfe7b964944bb169ca480a774b2795c3a
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2019-06-02 19:57:23 -0400

    Update yp_scrape.R

--- yp_scrape.R
+++ yp_scrape.R
@@ -29,7 +29,6 @@ yp_scrape<-function(link){
   ##########################
   ###Required Info########
   n<-get_page_n(link)
-  n<-n+1
   
   
   newurl<-paste(link,"&page=")
@@ -45,22 +44,15 @@ yp_scrape<-function(link){
   hrfs<-sapply(newurls, function(x) pipelink(x,"#main-content .business-name"))
   mainlink<-"https://www.yellowpages.com"
   goodlinks<-lapply(hrfs,function(y) unname(sapply(y,function(x) gsub(" ","",paste(mainlink,x)))))
+  goodlinks<-unname(goodlinks)
+  goodlinks<-unlist(goodlinks)
   
   ########Scraping########
-  nms<-lapply(goodlinks,function(y) unname(sapply(y,function(x) pipeit(x,"h1"))))
-  adr<-lapply(goodlinks, function(y) unname(sapply(y,function(x) pipeit(x, ".address"))))
-  pn<-lapply(goodlinks, function(y) unname(sapply(y,function(x) pipeit(x, ".phone"))))
-  wb<-lapply(goodlinks, function(y) unname(sapply(y ,function(x) pipelink(x, ".website-link"))))
-  eml<-lapply(goodlinks, function(y) unname(sapply(y,function(x)pipelink(x,".email-business"))))
+  nms<-sapply(goodlinks,function(x) pipeit(x,"h1"))
+  adr<-sapply(goodlinks,function(x) pipeit(x, ".address"))
+  pn<-sapply(goodlinks,function(x) pipeit(x, ".phone"))
+  wb<-sapply(goodlinks ,function(x) pipelink(x, ".website-link"))
+  eml<-sapply(goodlinks,function(x)pipelink(x,".email-business"))
 
-  kr<-tibble(Name=nms,Address=adr,"Phone Number"=pn,Website=wb,"Email"=eml)
-  
-  #####Wrangling#####
-  Nm<-unlist(unname(lapply(kr$Name, function(y) lapply(y, function(x) if(identical(x, character(0))) NA_character_ else x))))
-  Ad<-unlist(unname(lapply(kr$Address, function(y) lapply(y, function(x) if(identical(x, character(0))) NA_character_ else x))))
-  Pn<-unlist(unname(lapply(kr$Address, function(y) lapply(y, function(x) if(identical(x, character(0))) NA_character_ else x)))) 
-  Web<-unlist(unname(lapply(kr$Website, function(y) lapply(y, function(x) if(identical(x, character(0))) NA_character_ else x))))
-  Email<-kr$Email
-                   
-  tibble(Name=Nm,Address=Ad,"Phone Number"=Pn,Website=Web,Email)
+  tibble(Name=nms,Address=adr,"Phone Number"=pn,Website=wb,"Email"=eml)
 }
