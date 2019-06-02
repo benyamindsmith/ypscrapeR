0b3a4640ce5d414310d9f6c03af2cdcc49b0d28c
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2019-06-02 18:23:15 -0400

    Update yp_scrape.R

--- yp_scrape.R
+++ yp_scrape.R
@@ -1,6 +1,9 @@
 
 
 
+###The function###
+require(rvest)
+require(tibble)
 yp_scrape<-function(link){
   
   ###Required functions#####
@@ -24,17 +27,40 @@ yp_scrape<-function(link){
     n
   }
   ##########################
-  ###Required Info#####
-  mainlink<-"https://www.yellowpages.com"
+  ###Required Info########
   n<-get_page_n(link)
-  hrfs<-pipelink(link,"#main-content .business-name")
-  goodlinks<-unname(sapply(hr,function(x) gsub(" ","",paste(ml,x))))
-  nms<-unname(sapply(goodlinks,function(x) pipeit(x,"h1")))
-  adr<-unname(sapply(goodlinks,function(x) pipeit(x, ".address")))
-  pn<-unname(sapply(goodlinks,function(x) pipeit(x, ".phone")))
-  wb<-unname(sapply(goodlinks,function(x) pipelink(x, ".website-link")))
-  eml<-unname(sapply(goodlinks,function(x)pipelink(x,".email-business")))
+  n<-n+1
   
-  #Output
-  tibble(Name=nms,Address=adr,"Phone Number"=pn,Website=wb,"Email"=eml)
+  
+  newurl<-paste(link,"&page=")
+  newurl<-gsub(" ","",newurl)
+  
+  newurls<-c()
+  for(i in 1:n){
+    newurls[i]<-paste(newurl,i)
+  }
+  
+  newurls<-gsub(" ","",newurls)
+
+  hrfs<-sapply(newurls, function(x) pipelink(x,"#main-content .business-name"))
+  mainlink<-"https://www.yellowpages.com"
+  goodlinks<-lapply(hrfs,function(y) unname(sapply(y,function(x) gsub(" ","",paste(mainlink,x)))))
+  
+  ########Scraping########
+  nms<-lapply(goodlinks,function(y) unname(sapply(y,function(x) pipeit(x,"h1"))))
+  adr<-lapply(goodlinks, function(y) unname(sapply(y,function(x) pipeit(x, ".address"))))
+  pn<-lapply(goodlinks, function(y) unname(sapply(y,function(x) pipeit(x, ".phone"))))
+  wb<-lapply(goodlinks, function(y) unname(sapply(y ,function(x) pipelink(x, ".website-link"))))
+  eml<-lapply(goodlinks, function(y) unname(sapply(y,function(x)pipelink(x,".email-business"))))
+
+  kr<-tibble(Name=nms,Address=adr,"Phone Number"=pn,Website=wb,"Email"=eml)
+  
+  #####Wrangling#####
+  Nm<-unlist(unname(lapply(kr$Name, function(y) lapply(y, function(x) if(identical(x, character(0))) NA_character_ else x))))
+  Ad<-unlist(unname(lapply(kr$Address, function(y) lapply(y, function(x) if(identical(x, character(0))) NA_character_ else x))))
+  Pn<-unlist(unname(lapply(kr$Address, function(y) lapply(y, function(x) if(identical(x, character(0))) NA_character_ else x)))) 
+  Web<-unlist(unname(lapply(kr$Website, function(y) lapply(y, function(x) if(identical(x, character(0))) NA_character_ else x))))
+  Email<-kr$Email
+                   
+  tibble(Name=Nm,Address=Ad,"Phone Number"=Pn,Website=Web,Email)
 }
