e6511ea00057743fcb0d8dfe209947d83f46fbad
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2019-06-02 13:51:24 -0400

    Update and rename yp_scrape to yp_scrape.R

--- yp_scrape.R
+++ yp_scrape.R
@@ -1,4 +1,6 @@
 
+
+
 yp_scrape<-function(link){
   
   ###Required functions#####
@@ -9,8 +11,22 @@ yp_scrape<-function(link){
   pipeit<-function(url,code){
     read_html(url)%>%html_nodes(code)%>%html_text()
   }
+  
+  
+  get_page_n<-function(url){
+    n<-pipeit(url,".pagination")
+    n<-gsub("[A-Z]|[a-z]"," ",n)
+    n<-strsplit(n,"     ")
+    n<-unlist(n)
+    n<-as.numeric(n[2])
+    p<-length(pipelink(url,"#main-content .business-name"))
+    n<-ceiling(n/p)
+    n
+  }
   ##########################
+  ###Required Info#####
   mainlink<-"https://www.yellowpages.com"
+  n<-get_page_n(link)
   hrfs<-pipelink(link,"#main-content .business-name")
   goodlinks<-unname(sapply(hr,function(x) gsub(" ","",paste(ml,x))))
   nms<-unname(sapply(goodlinks,function(x) pipeit(x,"h1")))
